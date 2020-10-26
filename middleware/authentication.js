1fcafe75a34b2678801bba62e13f3aa4fbe9f2ce
Haymanot W <haymanotalemu21@gmail.com>
2020-10-26 10:13:06 -0400

    user authentication created

---
+++ middleware/authentication.js
@@ -0,0 +1,10 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    const token = req.header("authentication-token");
+    const decoded = jwt.verify(token, config.get("jsonWebTokenSecret"));
+    req.user = decoded.user;
+    // it goes to another middleware
+    next();
+};
