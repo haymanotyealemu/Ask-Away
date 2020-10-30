af219ae70ed558be638f6d5ee1bac8214b4229f4
Haymanot W <haymanotalemu21@gmail.com>
2020-10-30 11:30:12 -0400

    get all users action created

---
+++ client/src/actions/users.actions/getUsers.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { GET_USERS, USER_ERROR} from "../../constants/users.contants";
+
+export const getUser = () => async (dispatch) => {
+    try {
+        const res = await axios.get(`http://localhost:7000/api/users/allusers`);
+        dispatch({ type: GET_USERS, payload: res.data});
+    } catch (error) {
+        dispatch({
+            type: USER_ERROR,
+            payload: error,
+        });
+    }
+}
