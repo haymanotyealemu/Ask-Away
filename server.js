8ecf9bf9f26ab18c1dce30da62e415edb913f047
Haymanot W <haymanotalemu21@gmail.com>
2020-11-07 12:39:16 -0500

    setup the app for deploying

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const connectToDatabase = require("./config/connectToDatabase");
 
 const cors = require("cors");
@@ -18,11 +18,16 @@ app.use(express.json());
 app.use("/api/users", require("./routes/users.js"));
 app.use("/api/posts", require("./routes/posts.js"));
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ask-away");
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
 // Start the API server
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
