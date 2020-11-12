ff95858ccd6cec83d7d1ad6dfbd1fa39e2dc6ab3
Can Do <Katrinarhicks@gmail.com>
2020-11-12 09:50:31 -0500

    updated codes

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -7,22 +7,22 @@ const LandingPage = () => {
     <div className="landing-wrapper">
       <div className="text-wrapper">
         <div className="text-header-wrapper">
-          <p className="text-header font__p p__size">Welcome in</p>
+          <p className="text-header font__p p__size">Welcome to</p>
           <span className="larger">Ask Away! </span>
         </div>
 
         <div className="text-section font__p p__size">
-        <span className='app_color_font style'>Ask Away!</span> is a new forum about achieving success
+        <span className='app_color_font style'>Ask Away!</span> is a new forum about achieving success!
           <br />
-          If You are looking for answers on questions like:
+          If you are looking for answers to questions like:
           <ul>
-            <li>How to start my business?</li>
-            <li>How to grow company?</li>
+            <li>How to start a business?</li>
+            <li>How to grow your company?</li>
             <li>How to improve yourself every day</li>
-            <li>or You just want to share Your story</li>
+            <li>or if you just want to share your story</li>
           </ul>
           <div className="text-button-wrapper">
-            <Link to="/register">Register in</Link> and add post!
+            <Link to="/register">Register</Link> and add a post!
           </div>
         </div>
       </div>
